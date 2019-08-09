a4c33eb59e6583cfa6e28e25ba52e6138f36e2b3
Franciscob95 <fjbarros1@miuandes.cl>
2019-08-09 10:20:48 -0400

    tercer borrador

--- Card/Card/Board.cs
+++ Card/Card/Board.cs
@@ -8,6 +8,21 @@ namespace Card
 {
     class Board
     {
+        public List<CombatCard> meleeCards, rangeCards, longRangeCards;
+        public List<SpecialCard> captainCards, weatherCards;
+        public SpecialCard[] specialMeleeCards, specialLongRangeCards, specialRangeCards;
+        
+        public Board(List<CombatCard> meleeCards, List<CombatCard> rangeCards, List<CombatCard> longRangeCards, List<SpecialCard> captainCards, List<SpecialCard> weatherCards, SpecialCard[] specialMeleeCards, SpecialCard[] specialLongRangeCards, SpecialCard[] specialRangeCards)
+        {
+            this.meleeCards = meleeCards;
+            this.rangeCards = rangeCards;
+            this.longRangeCards = longRangeCards;
+            this.captainCards = captainCards;
+            this.weatherCards = weatherCards;
+            this.specialLongRangeCards = specialLongRangeCards;
+            this.specialMeleeCards = specialMeleeCards;
+            this.specialRangeCards = specialRangeCards;
+        }
 
     }
 }
